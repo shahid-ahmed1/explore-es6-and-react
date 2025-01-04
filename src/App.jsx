c81af3f3ce963596ca4b642441d9a8c8475c1e75
shahid-ahmed1 <shahidcse2122gmail,com>
2025-01-04 20:54:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Watch from './components/Watch/Watch'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <Watch></Watch>
+      
+    </>
+  )
+}
+
+export default App
