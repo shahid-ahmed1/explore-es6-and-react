b3a36c23cd98612711a383a13b05e1c0c5815324
shahid-ahmed1 <shahidcse2122gmail,com>
2025-01-04 22:52:30 +0600

    complited the watch data load

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 
+import { useState } from 'react'
 import './App.css'
 import Watch from './components/Watch/Watch'
+import { useEffect } from 'react'
+
 
 function App() {
- 
-
+  const [watches, setWatches]= useState([])
+  useEffect(()=>{
+    fetch('watches.json')
+    .then(res => res.json())
+    .then(data => setWatches(data))
+  },[])
+//  const watches=[
+//   {id:1,name:'apple watch',price:2000},
+//   {id:2,name:'svmsung watch',price:2000},
+//   {id:3,name: 'mi watch',price:2000}
+// ]
   return (
     <>
       
       <h1>Vite + React</h1>
-      <Watch></Watch>
+     {
+    watches.map(watch=> <Watch watch={watch} key={watch.id}></Watch>)
+     }
+     
       
     </>
   )
--- src/components/Watch/Watch.jsx
+++ src/components/Watch/Watch.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
 
-const Watch = () => {
+
+const Watch = ({watch}) => {
+     const {name , price }=watch
     return (
         <div>
-            
+            <h2>name: {name}</h2>
+            <h2>price: {price}</h2>
         </div>
     );
 };
